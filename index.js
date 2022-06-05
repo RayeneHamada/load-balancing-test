70fa4421e856d2837ef16f4d276a3736e0bfff84
Rayen Hamada <52611920+RayeneHamada@users.noreply.github.com>
2022-06-05 22:20:42 +0200

    Create Main Function

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const { networkInterfaces } = require('os');
+const port = 3000
+
+app.get('/', function (req, res) {
+    const nets = networkInterfaces();
+    const results = Object.create(null); // Or just '{}', an empty object
+
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+            // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+            const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+            if (net.family === familyV4Value && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            
+            }
+        }
+    }
+    res.send(results);
+});
+
+app.listen(port, () => {
+    console.log("it works")
+})
